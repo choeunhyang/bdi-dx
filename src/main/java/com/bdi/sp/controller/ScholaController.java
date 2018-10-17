46f9328c42b5077a1c0b58cf1cfc58a5c4b6ce08
dmsgi <dmsgi@DESKTOP-4GJ3BBF>
2018-10-17 21:40:45 +0900

    1017 0940

--- src/main/java/com/bdi/sp/controller/ScholaController.java
+++ src/main/java/com/bdi/sp/controller/ScholaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,13 +41,13 @@ public class ScholaController {
 	}
 	
 	@RequestMapping(value="/schola/{scid}",method=RequestMethod.DELETE)
-	public @ResponseBody String deleteschola(@ModelAttribute Schola sc,@PathVariable String scid){
+	public @ResponseBody String deleteschola(@RequestBody Schola sc,@PathVariable String scid){
 		logger.debug("Schola=>{}",sc);
 		return scs.deleteSchola(scid);
 	}
 	
 	@RequestMapping(value="/schola/{scid}",method=RequestMethod.PUT)
-	public @ResponseBody String updateschola(@ModelAttribute Schola sc,@PathVariable String scid){
+	public @ResponseBody String updateschola(@RequestBody Schola sc,@PathVariable String scid){
 		logger.debug("Schola=>{}",sc);
 		return scs.updateSchola(scid);
 	}
